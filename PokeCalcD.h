199017a4c22b9ed4ca31022502697d947fcca034
FD（えふでぃ） <fd.dwmk@gmail.com>
2023-01-12 00:17:26 +0900

    PokeCalcD first commit

---
+++ PokeCalcD.h
@@ -0,0 +1,32 @@
+﻿
+// PokeCalcD.h : PROJECT_NAME アプリケーションのメイン ヘッダー ファイルです
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
+#endif
+
+#include "resource.h"		// メイン シンボル
+
+
+// CPokeCalcDApp:
+// このクラスの実装については、PokeCalcD.cpp を参照してください
+//
+
+class CPokeCalcDApp : public CWinApp
+{
+public:
+	CPokeCalcDApp();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+// 実装
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CPokeCalcDApp theApp;
---
+++ PokeCalcDDlg.h
@@ -0,0 +1,34 @@
+﻿
+// PokeCalcDDlg.h : ヘッダー ファイル
+//
+
+#pragma once
+
+
+// CPokeCalcDDlg ダイアログ
+class CPokeCalcDDlg : public CDialogEx
+{
+// コンストラクション
+public:
+	CPokeCalcDDlg(CWnd* pParent = nullptr);	// 標準コンストラクター
+
+// ダイアログ データ
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_POKECALCD_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV サポート
+
+
+// 実装
+protected:
+	HICON m_hIcon;
+
+	// 生成された、メッセージ割り当て関数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
