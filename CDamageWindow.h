1865c388887ee21b7c49d54adf7f7ddc2711e3a1
FD2 <non>
2023-05-16 07:43:11 +0900

    push test

--- CDamageWindow.h
+++ CDamageWindow.h
@@ -31,6 +31,32 @@ private:
 	CDatabase *m_database;
 	std::vector<CImage> m_img; // 画像の個数は決まってるので決め打ちの方が良い？(全18タイプ、テラスタイプ、ゲージの色）
 
+	// 画像の種類分だけインデックスを定義
+	static constexpr unsigned int IMAGENAME_TYPE_FLARE = 0;
+	/*
+	static constexpr unsigned int IMAGENAME_TYPE_X = 1;
+	static constexpr unsigned int IMAGENAME_TYPE_X = 2;
+	static constexpr unsigned int IMAGENAME_TYPE_X = 3;
+	static constexpr unsigned int IMAGENAME_TYPE_X = 4;
+	static constexpr unsigned int IMAGENAME_TYPE_X = 5;
+	static constexpr unsigned int IMAGENAME_TYPE_X = 6;
+	static constexpr unsigned int IMAGENAME_TYPE_X = 7;
+	static constexpr unsigned int IMAGENAME_TYPE_X = 8;
+	static constexpr unsigned int IMAGENAME_TYPE_X = 9;
+	static constexpr unsigned int IMAGENAME_TYPE_X = 10;
+	static constexpr unsigned int IMAGENAME_TYPE_X = 11;
+	static constexpr unsigned int IMAGENAME_TYPE_X = 12;
+	static constexpr unsigned int IMAGENAME_TYPE_X = 13;
+	static constexpr unsigned int IMAGENAME_TYPE_X = 14;
+	static constexpr unsigned int IMAGENAME_TYPE_X = 15;
+	static constexpr unsigned int IMAGENAME_TYPE_X = 16;
+	static constexpr unsigned int IMAGENAME_TYPE_X = 17;
+	static constexpr unsigned int IMAGENAME_TYPE_X = 18;
+	*/
+	// テラスタイプの場合はちょっとだけ変えた画像も用意したい…
+
+	static constexpr unsigned int IMAGENAME_GAUGE_GRAY = 36; // テラスタイプも18種類あるので、続きから
+
 protected:
 	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV サポート
 
