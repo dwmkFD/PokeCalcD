5c29caccbc46953eec79b80d5c3b83dd54847936
FD2 <non>
2023-06-07 07:58:09 +0900

    asdf

--- pokemove.hpp
+++ pokemove.hpp
@@ -28,9 +28,9 @@ public:
 
 	void setRange( int range ) { m_range = range; }
 	static int getCategory( CString strCategory ) {
-		if ( strCategory == _T( "物理" ) ) return ( 1 );
-		else if ( strCategory == _T( "変化" ) ) return ( 0 );
-		else return ( 2 );
+		if ( strCategory == _T( "物理" ) ) return ( PHYSICS_CHECK );
+		else if ( strCategory == _T( "特殊" ) ) return ( SPECIAL_CHECK );
+		else return ( 0 );
 	}
 
 	// 技分類チェック
@@ -44,7 +44,7 @@ public:
 	int m_power; // 技の威力
 	int m_accuracy; // 技の命中率
 	int m_range; // 技の範囲（単体 4096、全体および相手全体 3072 → ダブルで威力の判定に使用する） // 3072で良いかどうかは要調査！
-	bool m_direct; // 接触攻撃か否か（鮫肌とかの反撃で落ちるかどうかを計算結果に含むなら）
+	bool m_direct; // 接触攻撃か否か（鮫肌とかの反撃で落ちるかどうかを計算結果に含むなら＆もふもふ等の判定に）
 	bool m_bite; // かみつき技か？
 	bool m_punch; // パンチ技か？
 	int m_critical; // 急所に当たりやすい技か？
@@ -112,12 +112,15 @@ struct TypeCompatibility {
 			= m_table[m_typetable[_T( "くさ" )]][m_typetable[_T( "ドラゴン" )]]
 			= m_table[m_typetable[_T( "くさ" )]][m_typetable[_T( "はがね" )]] = 0.5;
 
-		// こおり（弱点：くさ・じめん・ひこう・ドラゴン、半減：こおり、無効：なし）
+		// こおり（弱点：くさ・じめん・ひこう・ドラゴン、半減：ほのお、みず、こおり、はがね、無効：なし）
 		m_table[m_typetable[_T( "こおり" )]][m_typetable[_T( "くさ" )]]
 			= m_table[m_typetable[_T( "こおり" )]][m_typetable[_T( "じめん" )]]
 			= m_table[m_typetable[_T( "こおり" )]][m_typetable[_T( "ひこう" )]]
 			= m_table[m_typetable[_T( "こおり" )]][m_typetable[_T( "ドラゴン" )]] = 2;
-		m_table[m_typetable[_T( "こおり" )]][m_typetable[_T( "こおり" )]] = 0.5;
+		m_table[m_typetable[_T( "こおり" )]][m_typetable[_T( "ほのお" )]]
+			= m_table[m_typetable[_T( "こおり" )]][m_typetable[_T( "みず" )]]
+			= m_table[m_typetable[_T( "こおり" )]][m_typetable[_T( "こおり" )]]
+			= m_table[m_typetable[_T( "こおり" )]][m_typetable[_T( "はがね" )]] = 0.5;
 
 		// かくとう（弱点：ノーマル・こおり・いわ・あく・はがね、半減：どく・ひこう・エスパー・むし・フェアリー、無効：ゴースト）
 		m_table[m_typetable[_T( "かくとう" )]][m_typetable[_T( "ノーマル" )]]
@@ -166,11 +169,16 @@ struct TypeCompatibility {
 		m_table[m_typetable[_T( "エスパー" )]][m_typetable[_T( "あく" )]] = 0;
 
 		// むし（弱点：くさ・エスパー・あく、半減：ほのお・かくとう・どく・ひこう・ゴースト・はがね・フェアリー、無効：なし）
-		m_table[m_typetable[_T( "ひこう" )]][m_typetable[_T( "くさ" )]]
-			= m_table[m_typetable[_T( "ひこう" )]][m_typetable[_T( "かくとう" )]]
-			= m_table[m_typetable[_T( "ひこう" )]][m_typetable[_T( "むし" )]] = 2;
-		m_table[m_typetable[_T( "ひこう" )]][m_typetable[_T( "いわ" )]]
-			= m_table[m_typetable[_T( "ひこう" )]][m_typetable[_T( "はがね" )]] = 0.5;
+		m_table[m_typetable[_T( "むし" )]][m_typetable[_T( "くさ" )]]
+			= m_table[m_typetable[_T( "むし" )]][m_typetable[_T( "エスパー" )]]
+			= m_table[m_typetable[_T( "むし" )]][m_typetable[_T( "あく" )]] = 2;
+		m_table[m_typetable[_T( "むし" )]][m_typetable[_T( "ほのお" )]]
+			= m_table[m_typetable[_T( "むし" )]][m_typetable[_T( "かくとう" )]]
+			= m_table[m_typetable[_T( "むし" )]][m_typetable[_T( "どく" )]]
+			= m_table[m_typetable[_T( "むし" )]][m_typetable[_T( "ひこう" )]]
+			= m_table[m_typetable[_T( "むし" )]][m_typetable[_T( "ゴースト" )]]
+			= m_table[m_typetable[_T( "むし" )]][m_typetable[_T( "はがね" )]]
+			= m_table[m_typetable[_T( "むし" )]][m_typetable[_T( "フェアリー" )]] = 0.5;
 
 		// いわ（弱点：ほのお・こおり・ひこう・むし、半減：かくとう・じめん・はがね、無効：なし）
 		m_table[m_typetable[_T( "いわ" )]][m_typetable[_T( "ほのお" )]]
